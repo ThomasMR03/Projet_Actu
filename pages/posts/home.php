ff1686029c98651798b5d99c7ef0fa8d202f934a
Ansion Quentin <ansion.quentin@outlook.fr>
2017-04-05 11:00:15 +0200

    Add files via upload

--- pages/posts/home.php
+++ pages/posts/home.php
@@ -1,12 +1,12 @@
-<div class="row" style="margin:0;padding:;margin-top: 50px;">
+<div class="row" style="margin:0;padding:0;">
 	<div class="col-md-1">
 		
 	</div>
-	<div class="col-md-11" style="background:rgba(0,0,0,0.7);">
+	<div class="col-md-7" style="background:rgba(0,0,0,0.7);">
 		<div class="row">
 		<?php foreach (App::getInstance()->getTable('post')->last() as $post) : ?>
 			<div id="article" class="col-md-6">
-			<div class="imageArticle"><img src="img/imageteste.png"></div>
+			<div class="imageArticle"><img src="img/news.jpg"></div>
 			<div class="contenuArticle">
 			<h2><a href="<?= $post->Url ?>"> <?= $post->titre ?> </a></h2>
 			<h4>Article posté par <?= $post->auteur ?> le <?= $post->date_creation ?></h4>
@@ -16,7 +16,8 @@
 		<?php endforeach; ?>
 		</div>
 	</div>
-	<div class="col-md-1">
-		
+	<div class="col-md-1"></div>
+	<div class="col-md-2" id="colonneGauche">
+		<h3>Articles récents</h3>
 	</div>
 </div>
