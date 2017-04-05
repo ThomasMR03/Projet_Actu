8d538c4cffbf5a0a4a1b6ca49f9f50ca0c8f905a
Millien Thomas <Thomas.Face03@gmail.com>
2017-04-05 15:55:57 +0200

    Update Forum

--- pages/forum/single.php
+++ pages/forum/single.php
@@ -11,11 +11,47 @@
 
 <div class="col-md-8">
 	<h1 id="grostitre_article"><?= $message->titre; ?></h1>
+	<a href="index.php?p=forum.addMessage">Nouveau Message</a>
 	<?php foreach ($lastMessage as $last) : ?>
 	<div>
 	<p><?= $last->message; ?></p>
 	</div>
 	<?php endforeach; ?>
+	<?php
+	$app = App::getInstance();
+
+	if ($_POST) {
+		if (!empty($_POST['message'] && $_POST['sujet_id'])) {
+			$req = $app->getTable('message')->create(
+				["message"=>$_POST['message'],
+				"sujet_id"=>$_POST['sujet_id']
+				]);
+			if ($req) {
+				////message Flash
+				header('location: index.php?p=forum')
+				?>
+				<div class="alert alert-success">Bien enregistré</div>
+				<?php
+			}
+		}
+	}
+?>
+
+<div class="col-md-4">
+	
+</div>
+<div class="col-md-4">
+<h2 id="add_article">Ajouter un message</h2>
+<form method="post">
+	<input type="hidden" name="id" value="">
+	<input type="hidden" name="sujet_id" value="<?= $message->sujet_id ?>">
+	<textarea class="form-control" name="message" placeholder="Contenu Messages" ></textarea>
+	<input class="btn btn-warning" type="submit" name="">
+</form>
+</div>
+<div class="col-md-4">
+	
+</div>
 </div>
 <div class="col-md-4">
 	
