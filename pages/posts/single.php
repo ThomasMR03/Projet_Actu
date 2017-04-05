42b4af42b2e7fb14628090594afc75cd56c1fcf5
Ansion Quentin <ansion.quentin@outlook.fr>
2017-04-05 16:33:07 +0200

    Add files via upload

--- pages/posts/single.php
+++ pages/posts/single.php
@@ -6,12 +6,29 @@
 	}
 	$app->titre = $post->titre;
 ?>
-<div class="col-md-8">
+
+<div class="col-md-1"></div>
+<div class="col-md-7" id="grosArticle">
 	<h1 id="grostitre_article"><?= $post->titre; ?></h1>
-	<img src="<?=$post->img?>">
-	<p><?= $post->category; ?></p>
+	<img src="img/news.jpg">
+	<div class="texteGrosArticle">
+	<p style="text-align: center; color: darkgrey;"><?= $post->category; ?></p>
 	<p><?= $post->contenu; ?></p>
-</div>
-<div class="col-md-4">
+	<br><br>
+	<h6 style="color: grey;">Article posté par <?= $post->auteur ?> </h6>
+	</div>
 	
+
 </div>
+
+<div class="col-md-1"></div>
+	<div class="col-md-2" id="colonneGauche">
+		<h3 class="actu">Articles récents</h3>
+		<?php foreach (App::getInstance()->getTable('post')->lastRecent() as $post) : ?>
+			<div class="fondRecent">
+				<h5><a href="<?= $post->Url ?>"> <?= $post->titre ?> </a></h5>
+				<div class="imageArticleRecent"><img src="img/news.jpg"></div>
+				<p>Article posté par <?= $post->auteur ?> <br> Le <?= $post->date_creation_fr ?></p>
+			</div>
+		<?php endforeach; ?>
+	</div>
