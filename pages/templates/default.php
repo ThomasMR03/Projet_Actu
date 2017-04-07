884a6a35b2d49471172a4b552320453575be68ed
Ansion Quentin <ansion.quentin@outlook.fr>
2017-04-07 13:25:19 +0200

    Update default.php

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -6,6 +6,7 @@ $date = date("d-m-Y");
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0" />
     <link rel="icon" type="image/png" href="img/favico.png" />
     <meta name="description" content="">
     <meta name="author" content="">
