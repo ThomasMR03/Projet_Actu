5cda11a399437420f5882aab278292457d2710f7
Millien Thomas <Thomas.Face03@gmail.com>
2017-04-10 15:27:01 +0200

    Mail & Date Inscription

--- pages/utilisateurs/register.php
+++ pages/utilisateurs/register.php
@@ -1,13 +1,17 @@
 <?php
+$date = date('Y-m-d');
 
 if ($_POST) {
-if (isset($_POST['name'], $_POST['password'], $_POST['date_de_naissance']))
+if (isset($_POST['name'], $_POST['password'], $_POST['date_de_naissance'], $_POST['mail'], $_POST['date_inscription']))
  {
  	$req=App::getInstance()->getTable('User')->create([
 
  	'name' => $_POST['name'],
  	'password' => password_hash($_POST['password'], PASSWORD_BCRYPT),
+ 	'mail' => $_POST['mail'],
+ 	'date_inscription' => $_POST['date_inscription'],
  	'date_de_naissance' => $_POST['date_de_naissance']]);
+ 	
  	if ($req) {
 				header('location: index.php?p=Login');
 				?>
@@ -27,12 +31,14 @@ if (isset($_POST['name'], $_POST['password'], $_POST['date_de_naissance']))
 <div class="col-md-4" id="inscription">
  <h2>Inscription</h2>
  <form action="" method="post">
- 	<h3>Pseudonyme</h3>
+ 	<h3>Pseudonyme & Mail</h3>
  	<input type="text" name="name" placeholder="Votre Pseudo"><br>
+ 	<input type="text" name="mail" placeholder="Votre Mail">
  	<h3>Mot de passe</h3>
  	<input type="text" name="password" placeholder="Votre mot de passe"><br>
  	<h3>Date de naissance</h3>
- 	<input type="date" name="date_de_naissance" placeholder="Date de naissance"><br>
+ 	<input type="date" name="date_de_naissance"><br>
+ 	<input type="hidden" name="date_inscription" value="<?= $date ?>">
  	<button type="submit">Valider</button>
  </form>
  </div>
