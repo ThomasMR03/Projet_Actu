a2329f83125e5f6118f6347816375ec9dd79c15e
Ansion Quentin <ansion.quentin@outlook.fr>
2017-04-12 11:55:02 +0200

    Add files via upload

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -83,6 +83,32 @@ $date = date("d-m-Y");
 
 
 <?php include 'navbar_mobile.php' ?>
+
+
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+<script>
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#efefef",
+      "text": "#404040"
+    },
+    "button": {
+      "background": "#8ec760",
+      "text": "#ffffff"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "message": "Roubl'Actu utilise un cookies pour que vous puissiez rester connecté.",
+    "dismiss": "J’accepte !",
+    "link": "En savoir plus"
+  }
+})});
+</script>
+
 </body>
                 <!-- SCRIPTS LOADS -->
 <!-- <script type="text/javascript" hrel="scripts/bootstrap.min.js"></script>
