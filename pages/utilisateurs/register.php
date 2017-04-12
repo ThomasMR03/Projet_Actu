722d005532c9b9f145afc1af1f1d18a3fbad7420
Millien Thomas <Thomas.Face03@gmail.com>
2017-04-12 09:47:26 +0200

    Confirmation de mot de passe

--- pages/utilisateurs/register.php
+++ pages/utilisateurs/register.php
@@ -2,9 +2,12 @@
 $date = date('Y-m-d');
 
 if (!empty($_POST)) {
-if (isset($_POST['name'], $_POST['password'], $_POST['date_de_naissance'], $_POST['mail'], $_POST['date_inscription'])){
+if (isset($_POST['name'], $_POST['password'], $_POST['password_confirm'], $_POST['date_de_naissance'], $_POST['mail'], $_POST['date_inscription'])){
 	if(empty($_POST['name']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['name'])){
 			echo "Votre pseudo n'est pas valide (alpha_numerique)"; //Echo pour pseudo non valide
+		}else{
+	if(empty($_POST['password']) || $_POST['password']!=$_POST['password_confirm']){
+			echo "Les mots de passe ne correspondent pas"; //Echo pour mot de passe non valide
 		}else{
  	$req=App::getInstance()->getTable('User')->create([
 
@@ -17,6 +20,7 @@ if (isset($_POST['name'], $_POST['password'], $_POST['date_de_naissance'], $_POS
 				header('location: index.php?p=Login');
 				}
 		}
+	}
  	}
  }
 ?>
@@ -36,6 +40,7 @@ if (isset($_POST['name'], $_POST['password'], $_POST['date_de_naissance'], $_POS
  	<input type="text" name="mail" placeholder="Votre Mail">
  	<h3>Mot de passe</h3>
  	<input type="text" name="password" placeholder="Votre mot de passe"><br>
+ 	<input type="text" name="password_confirm" placeholder="Confirmer votre mot de passe"></br>
  	<h3>Date de naissance</h3>
  	<input type="date" name="date_de_naissance"><br>
  	<input type="hidden" name="date_inscription" value="<?= $date ?>">
