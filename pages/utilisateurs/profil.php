5868ec9390cd0153513de8fbe65689415ed5bbae
Ansion Quentin <ansion.quentin@outlook.fr>
2017-04-12 15:08:32 +0200

    Add files via upload

--- pages/utilisateurs/profil.php
+++ pages/utilisateurs/profil.php
@@ -3,8 +3,32 @@
 <?php endif; ?>
 
 
-<?= $_SESSION['Auth'],  $_SESSION['Rang'], $_SESSION['Age'] ?>
 
+
+<div class="row" style="width: 99%;">
+
+	<div class="col-md-2"></div>
+
+
+	<div class="col-md-8" id="pagePerso">
+		<div class="row">
+
+		<div class="col-md-3" id="imageProfil">
+			<img src="img/profil.png">
+		</div>
+
+		<div class="col-md-9">
+			<h1 id="pseudo">Bienvenue <?= $_SESSION['Auth']  ?></h1>
+		</div>
+
+
+		<div class="col-md-12">
+			<h6>Vous êtes : <?= $_SESSION['Rang']  ?></h6>
+			<h6>Date de naissance : <?= $_SESSION['Age']  ?></h6>
+		</div>
+	</div>
+
+</div>
 <!-- Mettre en page les sessions -->
 <!-- Un endroit pour mettre une photo de profil -->
 <!-- Un endroit pour mettre serveur, niveau & classe du joueur -->
