639094e92c956ec6f9eb75327ba628c6d01efae9
Ansion Quentin <ansion.quentin@outlook.fr>
2017-04-13 11:43:31 +0200

    Add files via upload

--- pages/utilisateurs/profil.php
+++ pages/utilisateurs/profil.php
@@ -9,7 +9,7 @@ $utilisateurs = App::getInstance()->getTable('User')->find($_SESSION['Id']);
 
 if (!empty($_POST['mail'])) {
 $req=App::getInstance()->getTable('User')->update($_SESSION['Id'],[
- 	'mail' => $_POST['image'],
+ 	'mail' => $_POST['mail'],
 ]); 
 }
 
@@ -37,36 +37,41 @@ if ($req) {
 		<div class="row">
 
 		<div class="col-md-3" id="imageProfilPerso">
-			<img src="img/imageProfil/<?= $utilisateurs->image ?>">
+			<img src="img/imageProfil/<?= $utilisateurs->image ?>.png">
+
+			<button onclick="formImage()" id="buttonFormImage">Modifier mon image</button>
+			<form action="" method="post" name="formulaire" id="formImage" class="formImageHidden">
+				<?php foreach (App::getInstance()->getTable('Image')->all() as $image) : ?>
+					<input name="image" type="image" value="<?= $image->images ?>" id="profil1" src="img/imageProfil/<?= $image->images ?>.png" width="50">
+				<?php endforeach ?>
+			</form>
 		</div>
 
 		<div class="col-md-9">
 			<h1 id="pseudo">Bienvenue <?= $utilisateurs->name  ?></h1>
-			<h6>Date d'inscription : <?= $utilisateurs->date_inscription_fr  ?></h6>
+			<h6><span id="infoProfil">Date d'inscription :</span> <?= $utilisateurs->date_inscription_fr  ?></h6>
 		</div>
 
+	
 
-		<div class="col-md-12">
-			<h6>Vous êtes : <?= $utilisateurs->membre_rang  ?></h6>
-			<h6>Date de naissance : <?= $utilisateurs->date_naissance_fr  ?></h6>
-			<h6>Adresse mail : <?= $utilisateurs->mail  ?></h6>
+		<div class="col-md-9" id="profilInfoPerso">
+			<h6><span id="infoProfil">Vous êtes :</span> <?= $utilisateurs->membre_rang  ?></h6><br>
+			<h6><span id="infoProfil">Date de naissance :</span> <?= $utilisateurs->date_naissance_fr  ?></h6><br>
+			<h6><span id="infoProfil">Adresse mail :</span> <?= $utilisateurs->mail  ?></h6>
+			<button onclick="formMail()" id="buttonFormMail">Modifier</button>
+
+			<form action="" method="post" name="formulaire" id="formMail" class="formMailHidden">
+				<input type="email" required="required" name="mail" placeholder="Nouvelle adresse Mail" onblur="verifMail(this)" data-errormessage='{"valueMissing": "Veuillez entrer une adresse mail valid"' style="float: left; width: 70%;">
+				<button type="submit" id="buttonFormMail">Valider</button>
+			</form>
 		</div>
 	</div>
 
 
 	<div class="col-md-12">
-		<form action="" method="post" name="formulaire">
-			<h6>Changer mon adresse mail</h6>
-			<input type="email" required="required" name="mail" placeholder="Votre Mail" onblur="verifMail(this)" data-errormessage='{"valueMissing": "Veuillez entrer une adresse mail valid"' class="form-control">
-			<button type="submit">Valider</button>
-		</form>
+		
 
-		<form action="" method="post" name="formulaire">
-			<h6>Changer mon image de profil</h6>
-			<?php foreach (App::getInstance()->getTable('Image')->all() as $image) : ?>
-			<input name="image" type="image" value="<?= $image->images ?>" id="profil1" src="img/imageProfil/<?= $image->images ?>" width="50">
-		<?php endforeach ?>
-		</form>
+		
 	</div>
 
 </div>
