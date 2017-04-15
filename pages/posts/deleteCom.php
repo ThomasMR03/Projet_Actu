4cdd83bcd6e5c4f2f68e8c2534d4a846ed2afe02
Millien Thomas <Thomas.Face03@gmail.com>
2017-04-15 11:05:12 +0200

    Update Admin

--- pages/posts/deleteCom.php
+++ pages/posts/deleteCom.php
@@ -1,3 +1,9 @@
+<?php $utilisateurs = App::getInstance()->getTable('User')->find($_SESSION['Id']); ?>
+<?php if(isset($_SESSION['Auth']) & $utilisateurs->membre_rang == 'admin'): ?>
+<?php else : header('location: index.php'); ?>
+<?php endif; ?>
+
+
 <?php App::getInstance()->getTable('commentaire')->delete($_POST['id']);
 
 $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
