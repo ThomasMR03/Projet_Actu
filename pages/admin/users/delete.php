26c6cd42b317c6ef20d0ac0dc2bd10c4c10dd373
Millien Thomas <Thomas.Face03@gmail.com>
2017-04-15 11:03:27 +0200

    Update Admin

--- pages/admin/users/delete.php
+++ pages/admin/users/delete.php
@@ -1,2 +1,8 @@
+<?php $utilisateurs = App::getInstance()->getTable('User')->find($_SESSION['Id']); ?>
+<?php if(isset($_SESSION['Auth']) & $utilisateurs->membre_rang == 'admin'): ?>
+<?php else : header('location: index.php'); ?>
+<?php endif; ?>
+
+
 <?php App::getInstance()->getTable('User')->delete($_POST['id']);
 header('location: admin.php?p=users.index') ?>
